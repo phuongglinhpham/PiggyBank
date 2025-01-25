042cbc95c7314462659d4bb1bd3882d29f078e83
Bianca Pham <qpham@mail.wlu.edu>
2025-01-25 17:47:37 -0500

    fixed bugs and improved error handling

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const userRouter = require("./routes/userRouter");
+const errorHandler = require("./middlewares/errorHandlerMiddleware");
 const app = express();
 
 
 //Connect to mongodb
 mongoose
-    .connect("mongodb+srv://phamthucquyen:Washingtonandlee1!@piggybank.jdvvd.mongodb.net/")
+    .connect('mongodb://localhost:27017/piggybank')
     .then(() => console.log("DB Connected"))
     .catch((e) => console.log(e));
 
@@ -14,7 +15,11 @@ mongoose
 app.use(express.json());  //Pass incoming json data
 //Routes
 app.use("/", userRouter);
+//Error
+app.use(errorHandler);
 
 //Start the server
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log(`Server is running on this port... ${PORT}`));
+app.listen(PORT, () =>
+    console.log(`Server is running on this port... ${PORT}`)
+);
--- backend/controllers/usersCtrl.js
+++ backend/controllers/usersCtrl.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../model/User");
 //User Registration
 
@@ -34,7 +35,74 @@ const usersController = {
         });
     }),
     //Login
+    login: asyncHandler(async (req, rec) => {
+        //Get the user data
+        const { email, password } = req.body;
+        //if email is correct
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new Error('Invalid login credentials');
+        }
+        //Compare the user password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            throw new Error('Invalid login credentials');
+        }
+        //Generate a token 
+        const token = jwt.sign({ id: user._id }, 'masynctechKey', {
+            expiresIn: "30d",
+        });
+        //Send the response
+        rec.json({
+            message: 'Login Success',
+            token,
+            id: user._id,
+            email: user.email,
+            username: user.username,
+        });
+    }),
     //profile
+    profile: asyncHandler(async (req, res) => {
+        //Find the user
+        console.log(req.user);
+        const user = await User.findById(req.user);
+        if (!user) {
+            throw new Error("User not found");
+        }
+        //Send the response
+        res.json({ username: user.username, email: user.email });
+    }),
+    //Change password
+    changeUserPassword: asyncHandler(async (req, res) => {
+        const { newPassword } = req.body
+        //Find the user
+        const user = await User.findById(req.user);
+        if (!user) {
+            throw new Error("User not found");
+        }
+        //Hash the new password before saving
+        //Hash the user password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(newPassword, salt);
+        user.password = hashedPassword;
+        //Resave
+        await user.save()
+        //Send the response
+        res.json({ message: "Password Changed successfully" });
+    }),
+    //Update user profile
+    updateUserProfile: asyncHandler(async (req, res) => {
+        const { email, username } = req.body;
+        const updatedUser = await User.findByIdAndUpdate(req.user, {
+            username,
+            email,
+        },
+            {
+                new: true,
+            }
+        );
+        res.json({ message: "User profile updated successfully", updatedUser });
+    }),
 };
 
 module.exports = usersController;
